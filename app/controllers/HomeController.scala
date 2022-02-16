79ba75eca79ddf188ad6f096eb87aadc3460e914
mywyau <47248104+mywyau@users.noreply.github.com>
2022-02-16 13:56:22 +0000

    refactor messages, keys, and unit tsts

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,17 +1,21 @@
 package controllers
 
-import javax.inject._
-import play.api._
+import play.api.i18n._
 import play.api.mvc._
 import views.html.indexView
+import play.api.i18n.Messages
+
+import javax.inject._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(indexView: indexView,
-                               val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(override val messagesApi: MessagesApi,
+                               indexView: indexView,
+                               val controllerComponents: MessagesControllerComponents,
+                              )() extends BaseController with I18nSupport {
 
   /**
    * Create an Action to render an HTML page.
@@ -22,8 +26,28 @@ class HomeController @Inject()(indexView: indexView,
    */
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(indexView())
+    Ok(indexView("placeholder string param"))
   }
 
 
+}
+
+object Runner extends App {
+
+  private def divideXByY(x: Int, y: Int): Either[String, Int] = {
+    if (y == 0) Left("Dude, not cool cannot divide by 0")
+    else Right(x / y)
+  }
+
+  def printSomething = {
+    println(divideXByY(1, 0))
+    println(divideXByY(1, 1))
+
+    divideXByY(1, 0) match {
+      case Left(s) => println("Answer: " + s)
+      case Right(i) => println("Answer: " + i)
+    }
+  }
+
+  printSomething
 }
