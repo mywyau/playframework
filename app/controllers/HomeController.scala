8c67b49bd00371f7c9a04eaac1ef05402d213873
mywyau <47248104+mywyau@users.noreply.github.com>
2022-02-16 18:02:57 +0000

    wip form errors, bad request views, next page controller and submission

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,9 +1,10 @@
 package controllers
 
+import forms.HomeForm
+import model.UserData
 import play.api.i18n._
 import play.api.mvc._
-import views.html.indexView
-import play.api.i18n.Messages
+import views.html._
 
 import javax.inject._
 
@@ -14,6 +15,8 @@ import javax.inject._
 @Singleton
 class HomeController @Inject()(override val messagesApi: MessagesApi,
                                indexView: indexView,
+                               badView: badView,
+                               formProvider: HomeForm,
                                val controllerComponents: MessagesControllerComponents,
                               )() extends BaseController with I18nSupport {
 
@@ -25,29 +28,43 @@ class HomeController @Inject()(override val messagesApi: MessagesApi,
    * a path of `/`.
    */
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(indexView("placeholder string param"))
+  def onPageLoad() = Action { implicit request: Request[AnyContent] =>
+    Ok(indexView(formProvider(), "placeholder string param", routes.HomeController.onSubmit()))
+  }
+
+  def onSubmit() = Action { implicit request: Request[AnyContent] =>
+
+    formProvider().bindFromRequest.fold(
+      formWithErrors =>
+        BadRequest(badView(formWithErrors, "bad request", routes.HomeController.onPageLoad())),
+      userData => {
+        /* binding success, you get the actual value. */
+        val newUser = UserData(userData.name, userData.age)
+        val name = newUser.name
+        Redirect(routes.NextPageController.onSubmit())
+      }
+    )
   }
 
 
 }
 
-object Runner extends App {
-
-  private def divideXByY(x: Int, y: Int): Either[String, Int] = {
-    if (y == 0) Left("Dude, not cool cannot divide by 0")
-    else Right(x / y)
-  }
-
-  def printSomething = {
-    println(divideXByY(1, 0))
-    println(divideXByY(1, 1))
-
-    divideXByY(1, 0) match {
-      case Left(s) => println("Answer: " + s)
-      case Right(i) => println("Answer: " + i)
-    }
-  }
-
-  printSomething
-}
+//object Runner extends App {
+//
+//  private def divideXByY(x: Int, y: Int): Either[String, Int] = {
+//    if (y == 0) Left("Dude, not cool cannot divide by 0")
+//    else Right(x / y)
+//  }
+//
+//  def printSomething = {
+//    println(divideXByY(1, 0))
+//    println(divideXByY(1, 1))
+//
+//    divideXByY(1, 0) match {
+//      case Left(s) => println("Answer: " + s)
+//      case Right(i) => println("Answer: " + i)
+//    }
+//  }
+//
+//  printSomething
+//}
---
+++ app/model/UserData.scala
@@ -0,0 +1,3 @@
+package model
+
+case class UserData(name: String, age: Int)
